437cca811f3c6a46659497c78a7504eba06cb660
Malkolm Alburquenque <malkolm.alburquenque@mail.mcgill.ca>
2016-10-13 17:15:18 -0400

    update

--- Web/FoodTruckManagementSystem/controller/InputValidator.php
+++ Web/FoodTruckManagementSystem/controller/InputValidator.php
@@ -4,9 +4,10 @@ class InputValidator{
 		$data = trim($data);
 		$data = stripslashes($data);
 		$data = htmlspecialchars($data);
-		if (is_string($data))
+		if (is_string($data)){
 			$data = strtolower($data); //ensures all strings are stored as lower case
+		}
 		return $data;
 	}
 }
-?>
+?> 
