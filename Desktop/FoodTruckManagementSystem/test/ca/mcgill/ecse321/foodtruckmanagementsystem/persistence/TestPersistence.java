cbfd7ed2d9d72f911aad4dcf649299acdabe2219
Soqueen <sokhenglim973@gmail.com>
2016-10-13 21:20:07 -0400

    add view

---
+++ Desktop/FoodTruckManagementSystem/test/ca/mcgill/ecse321/foodtruckmanagementsystem/persistence/TestPersistence.java
@@ -0,0 +1,61 @@
+package ca.mcgill.ecse321.foodtruckmanagementsystem.persistence;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.mcgill.ecse321.foodtruckmanagementsystem.model.Equipment;
+import ca.mcgill.ecse321.foodtruckmanagementsystem.model.Manager;
+import ca.mcgill.ecse321.foodtruckmanagementsystem.model.Order;
+import ca.mcgill.ecse321.foodtruckmanagementsystem.model.StaffMember;
+import ca.mcgill.ecse321.foodtruckmanagementsystem.model.Supply;
+
+public class TestPersistence {
+
+	@Before
+	public void setUp() throws Exception {
+		Manager manager = Manager.getInstance();
+		
+		Equipment equipment1 = new Equipment("grill", 2);
+		Equipment equipment2 = new Equipment("fridge", 1);
+		
+		manager.addEquipment(equipment1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Manager manager = Manager.getInstance();
+		manager.delete();
+	}
+
+	@Test
+	public void test() {
+		Manager manager = Manager.getInstance();
+		PersistenceXStream.setFilename("test"+File.separator+"ca"+File.separator+"mcgill"+File.separator+"ecse321"
+										+File.separator+"foodtruckmanagementsystem"+File.separator+"persistence"+File.separator
+										+File.separator+"data.xml");
+		PersistenceXStream.setAlias("equipment", Equipment.class);
+		PersistenceXStream.setAlias("manager", Manager.class);
+		PersistenceXStream.setAlias("order", Order.class);
+		PersistenceXStream.setAlias("staffmember", StaffMember.class);
+		PersistenceXStream.setAlias("supply", Supply.class);
+		if(!PersistenceXStream.saveToXMLwithXStream(manager))
+			fail("Could not save file.");
+		
+		manager.delete();
+		assertEquals(0, manager.getEquipments().size());
+		
+		manager = (Manager) PersistenceXStream.loadFromXMLwithXStream();
+		if(manager == null)
+			fail("Could not load file.");
+		
+		assertEquals(1, manager.getEquipments().size());
+		assertEquals("grill", manager.getEquipment(0).getName());
+		assertEquals(2, manager.getEquipment(0).getQuantity());		
+	}
+
+}
