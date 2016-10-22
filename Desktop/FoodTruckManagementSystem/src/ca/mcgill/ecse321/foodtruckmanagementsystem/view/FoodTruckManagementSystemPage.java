95caf7235d938b451cb5440f8f5e7888426a9f38
b300631 <Brennan.Laforge@mail.mcgill.ca>
2016-10-22 12:32:47 -0400

    Updated FTMS Page
    Added a JScrollPane for adding/removing components

--- Desktop/FoodTruckManagementSystem/src/ca/mcgill/ecse321/foodtruckmanagementsystem/view/FoodTruckManagementSystemPage.java
+++ Desktop/FoodTruckManagementSystem/src/ca/mcgill/ecse321/foodtruckmanagementsystem/view/FoodTruckManagementSystemPage.java
@@ -1,70 +1,91 @@
 package ca.mcgill.ecse321.foodtruckmanagementsystem.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import ca.mcgill.ecse321.foodtruckmanagementsystem.controller.InvalidInputException;
 import ca.mcgill.ecse321.foodtruckmanagementsystem.controller.ItemController;
-import ca.mcgill.ecse321.foodtruckmanagementsystem.controller.ItemController;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JSpinner;
 import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Toolkit;
-import java.awt.Color;
-import java.awt.Component;
-
-import javax.swing.JSpinner;
-import javax.swing.JTabbedPane;
-import javax.swing.SpinnerModel;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.Toolkit;
 
 public class FoodTruckManagementSystemPage extends JFrame {
 
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField equipmentName;
-	private JLabel errorMessage, equipmentLabel;
-	private JSpinner equipmentQuantity;
+	private String error = "";
+	private JLabel errorMessage;
 	private SpinnerNumberModel model;
-	private int equipmentCount = 0;
-	private String error = null;
 
-	public FoodTruckManagementSystemPage(){
+	/**
+	 * Launch the application.
+	 */
+	public FoodTruckManagementSystemPage() {
 		initComponents();
 		refreshData();
 	}
 
+	/**
+	 * Create the frame.
+	 */
 	public void initComponents() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(FoodTruckManagementSystemPage.class.getResource("/ca/mcgill/ecse321/foodtruckmanagementsystem/view/foodtruck.png")));
-		setTitle("Food Truck Management Software");
+		setTitle("Food Truck Management System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1111, 303);
+		setBounds(100, 100, 1896, 955);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JTabbedPane tab = new JTabbedPane();
-		getContentPane().add(tab);
+		JScrollPane addremovePane = new JScrollPane();
+		addremovePane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		addremovePane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(addremovePane, GroupLayout.PREFERRED_SIZE, 1300, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(1154, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(addremovePane, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(557, Short.MAX_VALUE))
+		);
 		
-		errorMessage = new JLabel("");
+		JPanel panel = new JPanel();
+		addremovePane.setViewportView(panel);
+		
+		error = "";
+		errorMessage = new JLabel(error);
 		errorMessage.setForeground(Color.RED);
 		
-		equipmentLabel = new JLabel("Equipment:");
+		JLabel equipment = new JLabel("Equipment:");
 		
 		equipmentName = new JTextField();
 		equipmentName.setColumns(10);
+		
+		model = new SpinnerNumberModel(0, -1000000, 1000000, 1);
+		JSpinner equipmentQuantity = new JSpinner(model);
 		
 		JButton btnAddEquipment = new JButton("Add Equipment");
 		btnAddEquipment.addActionListener(new ActionListener() {
@@ -73,50 +94,66 @@ public class FoodTruckManagementSystemPage extends JFrame {
 			}
 		});
 		
-		model = new SpinnerNumberModel(0, -1000000, 1000000, 1);
-		equipmentQuantity = new JSpinner(model);		
-		
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+		JButton btnRemoveEquipment = new JButton("Remove Equipment");
+		btnRemoveEquipment.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//removeEquipmentActionPerformed(e);
+			}
+		});
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addComponent(errorMessage)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(equipmentLabel)
-							.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(equipment)
+							.addGap(11)
 							.addComponent(equipmentName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(equipmentQuantity, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 							.addGap(15)
-							.addComponent(btnAddEquipment)))
-					.addContainerGap(312, Short.MAX_VALUE))
+							.addComponent(equipmentQuantity, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addGap(15)
+							.addComponent(btnAddEquipment)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnRemoveEquipment)))
+					.addContainerGap(268, Short.MAX_VALUE))
 		);
 		
-		gl_contentPane.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnAddEquipment, equipmentQuantity});
-		gl_contentPane.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnAddEquipment, equipmentName});
+		gl_panel.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnAddEquipment, equipmentQuantity});
+		gl_panel.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnAddEquipment, equipmentName});
+		gl_panel.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnRemoveEquipment, equipmentQuantity});
+		gl_panel.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {btnRemoveEquipment, equipmentName});
 		
-		
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
 					.addComponent(errorMessage)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(equipmentLabel)
-						.addComponent(equipmentName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(equipmentQuantity, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAddEquipment))
-					.addContainerGap(136, Short.MAX_VALUE))
+					.addGap(5)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(4)
+							.addComponent(equipment))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(1)
+							.addComponent(equipmentName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(1)
+							.addComponent(equipmentQuantity, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+							.addComponent(btnAddEquipment)
+							.addComponent(btnRemoveEquipment)))
+					.addContainerGap(223, Short.MAX_VALUE))
 		);
+		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
+		
+		pack();
 	}
 	
 	private void refreshData()
 	{
 		errorMessage.setText(error);
 	}
-	 
 	
 	private void addEquipmentActionPerformed(java.awt.event.ActionEvent evt)
 	{
@@ -128,5 +165,19 @@ public class FoodTruckManagementSystemPage extends JFrame {
 				error = e.getMessage();
 			}
 		refreshData();
+	}
+	
+	private void removeEquipmentActionPerformed(java.awt.event.ActionEvent evt)
+	{
+		/*
+		ItemController c = new ItemController();
+		error = "";
+			try {
+				c.removeEquipment(equipmentName.getText(),  model.getNumber().intValue());
+			} catch (InvalidInputException e) {
+				error = e.getMessage();
+			}
+		refreshData();
+		*/
 	}
 }
