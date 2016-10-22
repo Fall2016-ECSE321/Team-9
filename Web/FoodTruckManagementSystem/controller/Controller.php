0445075cdf66e11a13029f98f6a1c8dcc37f2814
Malkolm Alburquenque <malkolm.alburquenque@mail.mcgill.ca>
2016-10-22 11:22:48 -0400

    desktop files plus webcontroller

--- Web/FoodTruckManagementSystem/controller/Controller.php
+++ Web/FoodTruckManagementSystem/controller/Controller.php
@@ -50,6 +50,53 @@ class Controller{
 			$error = trim($error);
 			throw new Exception ($error);
 		}	
+		
+	}	public function removeEquipment($equipment_name, $equipment_quantity){
+
+		$error = "";
+		$name= InputValidator::validate_input($equipment_name);
+		$quantity = InputValidator::validate_input($equipment_quantity);
+		if (($name != null || strlen($name)!=0)&&($quantity != null || $quantity !=0) &&($quantity>0)){
+			//1. Load all of the data
+			$pm = new PersistenceFoodTruckManagementSystem();
+			$m  = $pm->loadDataFromStore();
+				
+			for($i = 0; $i < $m->numberOfEquipments(); $i++){
+				if($name == $m->getEquipment_index($i)->getName()){
+					if($m->getEquipment_index($i)->getQuantity() > $quantity){
+						$m->getEquipment_index($i)->setQuantity((string)($m->getEquipment_index($i)->getQuantity() - $quantity));
+					}
+					if($m->getEquipment_index($i)->getQuantity == $quantity){
+						$m->getEquipment_index($i)->delete();
+					}
+					else{
+						$error .= "Equipment quantity is only: " + $m->getEquipment_index($i)->getQuantity ;
+					}
+				break;
+				}
+			}
+			if (! $flag){
+				//2. Add the new equipment
+				$equipment = new Equipment($name, $quantity);
+				$m->addEquipment($equipment);
+			}
+			//3. Write all of the data
+			$pm->writeDataToStore($m);
+		}
+		else{
+			// 4. Validate all the innputs 
+			if ($name == null || strlen($name)==0){
+				$error .="Equipment name cannot be empty!";
+			}
+			if ($quantity == null || $quantity == 0){
+				$error .= " Equipment quantity cannot be empty or zero!";
+			} 
+			if ($quantity < 0){
+				$error .= " Equipment quantity cannot be negative!";
+			}
+			$error = trim($error);
+			throw new Exception ($error);
+		}	
 	}
 	
 }
