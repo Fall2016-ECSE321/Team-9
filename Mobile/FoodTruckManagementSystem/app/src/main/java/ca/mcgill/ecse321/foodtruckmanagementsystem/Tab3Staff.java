7fadf5e79a2ebd3f8702eb246fc84eab7ff95d1a
EBruchet <evan.bruchet@mail.mcgill.ca>
2016-11-29 19:03:37 -0500

    Removed unnecessary lines of code throughout Android project

--- Mobile/FoodTruckManagementSystem/app/src/main/java/ca/mcgill/ecse321/foodtruckmanagementsystem/Tab3Staff.java
+++ Mobile/FoodTruckManagementSystem/app/src/main/java/ca/mcgill/ecse321/foodtruckmanagementsystem/Tab3Staff.java
@@ -23,13 +23,9 @@ import ca.mcgill.ecse321.foodtruckmanagementsystem.persistence.PersistenceXStrea
 
 public class Tab3Staff extends Fragment{
 
-    Manager m = Manager.getInstance();
     Spinner roleSpinner;
     ArrayAdapter<CharSequence> adapter;
-    Spinner nameSpinner;
-    ArrayAdapter<String> nameAdapter;
-    HashMap<Integer, StaffMember> staffmembers;
-    View v;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,20 +35,6 @@ public class Tab3Staff extends Fragment{
         adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.roles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         roleSpinner.setAdapter(adapter);
-
-
-        nameSpinner = (Spinner) rootView.findViewById(R.id.staffmember_spinner);
-        nameAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item);
-        nameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        this.staffmembers = new HashMap<Integer, StaffMember>();
-
-        int i = 0;
-        for (Iterator<StaffMember> staffmembers = m.getStaffmembers().iterator(); staffmembers.hasNext(); i++){
-            StaffMember sM = staffmembers.next();
-            nameAdapter.add(sM.getName());
-            this.staffmembers.put(i, sM);
-        }
-        nameSpinner.setAdapter(nameAdapter);
 
         return rootView;
     }
